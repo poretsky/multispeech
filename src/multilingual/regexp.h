10e2ed7fb39c60c658fee4af5fa90bc20273d30e
Igor B. Poretsky <poretsky@mlbox.ru>
2005-05-19 12:43:32 +0000

    C and C++ coexistence.

--- src/multilingual/regexp.h
+++ src/multilingual/regexp.h
@@ -8,7 +8,23 @@
 #ifndef REGEXP_H
 #define REGEXP_H 1
 
+/* BEGIN_C_DECLS should be used at the beginning of your declarations,
+   so that C++ compilers don't mangle their names.  Use END_C_DECLS at
+   the end of C declarations. */
+#undef BEGIN_C_DECLS
+#undef END_C_DECLS
+#ifdef __cplusplus
+# define BEGIN_C_DECLS extern "C" {
+# define END_C_DECLS }
+#else
+# define BEGIN_C_DECLS /* empty */
+# define END_C_DECLS /* empty */
+#endif
+
 #define NSUBEXP  10
+
+BEGIN_C_DECLS
+
 typedef struct regexp {
 	char *startp[NSUBEXP];
 	char *endp[NSUBEXP];
@@ -23,5 +39,7 @@ regexp *regcomp(char *);
 int regexec(regexp *, char *);
 void regsub(regexp *, char *, char *);
 void regerror(char *);
+
+END_C_DECLS
 
 #endif
--- src/multilingual/xmalloc.h
+++ src/multilingual/xmalloc.h
@@ -3,7 +3,24 @@
 #ifndef XMALLOC_H
 #define XMALLOC_H 1
 
+/* BEGIN_C_DECLS should be used at the beginning of your declarations,
+   so that C++ compilers don't mangle their names.  Use END_C_DECLS at
+   the end of C declarations. */
+#undef BEGIN_C_DECLS
+#undef END_C_DECLS
+#ifdef __cplusplus
+# define BEGIN_C_DECLS extern "C" {
+# define END_C_DECLS }
+#else
+# define BEGIN_C_DECLS /* empty */
+# define END_C_DECLS /* empty */
+#endif
+
+BEGIN_C_DECLS
+
 void *xrealloc(void *, unsigned int);
 void *xmalloc(unsigned int);
+
+END_C_DECLS
 
 #endif
