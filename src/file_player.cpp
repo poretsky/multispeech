82fe2542f6ac09919ded017807827f1531575acb
Igor B. Poretsky <poretsky@rakurs.com>
2019-10-31 16:58:22 +0300

    Removed redundant destructors

--- src/file_player.cpp
+++ src/file_player.cpp
@@ -38,17 +38,13 @@ sound_task::sound_task(const path& sound_file, float sound_volume):
 }
 
 
-// Construct / destroy:
+// Object constructor:
 
 file_player::file_player(const configuration& conf):
   soundfile(conf.option_value[options::sounds::device].as<string>().empty() ?
             conf.option_value[options::audio::device].as<string>() :
             conf.option_value[options::sounds::device].as<string>(),
             "sounds")
-{
-}
-
-file_player::~file_player(void)
 {
 }
 
