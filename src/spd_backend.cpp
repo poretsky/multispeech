4dbdc02fa8f62ebcf5dad674250f3097ab2f0a28
Igor B. Poretsky <poretsky@mlbox.ru>
2019-11-08 00:03:41 +0300

    Initial Speech Dispatcher commands execution
    Actually, only module initialization and exiting are implemented for
    the moment.

--- src/spd_backend.cpp
+++ src/spd_backend.cpp
@@ -19,8 +19,12 @@
 */
 
 #include <string>
+#include <iostream>
 
 #include "spd_backend.hpp"
+
+#include "strcvt.hpp"
+#include "config.hpp"
 
 using namespace std;
 
@@ -40,11 +44,15 @@ static const wstring cmd_loglevel = L"LOGLEVEL";
 static const wstring cmd_debug = L"DEBUG";
 static const wstring cmd_quit = L"QUIT";
 
+// Pseudocommand:
+static const wstring cmd_abort = L"ABORT";
+
 
 // Object construction:
 
 spd_backend::spd_backend(const configuration& conf):
-  server(conf)
+  server(conf),
+  initialized(false)
 {
 }
 
@@ -62,6 +70,9 @@ spd_backend::queue_done(void)
 void
 spd_backend::get_command(void)
 {
+  string s;
+  getline(cin, s);
+  cmd = (cin.eof() || cin.fail()) ? cmd_abort :  intern_string(s, input_charset);
 }
 
 
@@ -70,5 +81,27 @@ spd_backend::get_command(void)
 bool
 spd_backend::perform_command(void)
 {
-  return false;
+  if (initialized)
+    {
+      if (cmd_quit == cmd)
+        {
+          cout << "210 OK QUIT" << endl;
+          return false;
+        }
+      else if (cmd_abort == cmd)
+        return false;
+      else cout << "300 ERR UNKNOWN COMMAND" << endl;
+    }
+  else if (cmd_init == cmd)
+    {
+      initialized = true;
+      cout << "299-" << package::name << ": Initialized successfully." << endl;
+      cout << "299 OK LOADED SUCCESSFULLY" << endl;
+    }
+  else
+    {
+      cerr << "ERROR: Wrong communication from module client: didn't call INIT" << endl;
+      return false;
+    }
+  return true;
 }
