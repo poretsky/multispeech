fbdae76191df34386ed1e4e15c5d2049bec2328c
Igor B. Poretsky <poretsky@mlbox.ru>
2019-11-09 19:16:17 +0300

    IO error handling

--- src/spd_backend.cpp
+++ src/spd_backend.cpp
@@ -18,6 +18,7 @@
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
 */
 
+#include <cstdlib>
 #include <string>
 #include <iostream>
 #include <stdexcept>
@@ -44,9 +45,6 @@ static const wstring cmd_audio(L"AUDIO");
 static const wstring cmd_loglevel(L"LOGLEVEL");
 static const wstring cmd_debug(L"DEBUG");
 static const wstring cmd_quit(L"QUIT");
-
-// Pseudocommand:
-static const wstring cmd_abort(L"ABORT");
 
 
 // Object instantiation:
@@ -90,7 +88,12 @@ spd_backend::get_command(void)
 {
   string s;
   getline(cin, s);
-  cmd = (cin.eof() || cin.fail()) ? cmd_abort :  intern_string(s, input_charset);
+  if (cin.eof() || cin.fail())
+    {
+      exit_status = EXIT_FAILURE;
+      cmd = cmd_quit;
+    }
+  else cmd = intern_string(s, input_charset);
 }
 
 
@@ -101,11 +104,10 @@ spd_backend::perform_command(void)
 {
   if (cmd_quit == cmd)
     {
-      cout << "210 OK QUIT" << endl;
+      if (exit_status == EXIT_SUCCESS)
+        cout << "210 OK QUIT" << endl;
       return false;
     }
-  else if (cmd_abort == cmd)
-    return false;
   else cout << "300 ERR UNKNOWN COMMAND" << endl;
   return true;
 }
