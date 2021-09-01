239df1523a1222a1c9fb8804d0f6786eb3d3950f
Igor B. Poretsky <poretsky@mlbox.ru>
2021-09-02 00:37:20 +0300

    Convenient sysconfig inclusion

---
+++ src/core/sysconfig.hpp
@@ -0,0 +1,36 @@
+// sysconfig.hpp -- Convenient sysconfig inclusion
+/*
+   Copyright (C) 2021 Igor B. Poretsky <poretsky@mlbox.ru>
+   This file is part of Multispeech.
+
+   Multispeech is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   Multispeech is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Multispeech; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
+*/
+
+#ifndef MULTISPEECH_SYSCONFIG_HPP
+#define MULTISPEECH_SYSCONFIG_HPP
+
+#undef PACKAGE
+#undef PACKAGE_BUGREPORT
+#undef PACKAGE_NAME
+#undef PACKAGE_STRING
+#undef PACKAGE_TARNAME
+#undef PACKAGE_URL
+#undef PACKAGE_VERSION
+#undef VERSION
+
+#include <sysconfig.h>
+#undef VERSION
+
+#endif
