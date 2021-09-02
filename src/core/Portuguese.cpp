a0591bf3953363866ece6b0cb7d3d57a8de4330c
Igor B. Poretsky <poretsky@mlbox.ru>
2021-09-03 00:59:01 +0300

    Native Portuguese language name

--- src/core/Portuguese.cpp
+++ src/core/Portuguese.cpp
@@ -152,7 +152,7 @@ Portuguese::Portuguese(void):
     (L"dot", L"ponto")
 
     // Language name:
-    (intern_string(id, locale("")).c_str(), L"Brasileiro")
+    (intern_string(id, locale("")).c_str(), L"Português")
     .convert_to_container< map<const wstring, const wchar_t*> >();
 
   // Text filtering chain:
